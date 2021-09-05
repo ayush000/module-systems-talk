fad30051faf3c938ade56c6696dae6ea0f069a46
Ayush Sachdeva <ayush.sachdeva@helpshift.com>
2021-09-05 14:58:50 +0530

    11-babel

---
+++ 11-babel/babel.config.js
@@ -0,0 +1,11 @@
+const presets = [
+  [
+    "@babel/env",
+    {
+      modules: false
+    //   modules: "amd"
+    },
+  ],
+];
+
+module.exports = { presets };
---
+++ 11-babel/index.js
@@ -0,0 +1,6 @@
+const f = () => {
+    const str = "I am babel";
+    return f;
+}
+
+export default f;
