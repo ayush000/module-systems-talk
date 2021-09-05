c08d3580751dd303822e2bcffbdfafb823a5f9d2
Ayush Sachdeva <ayush.sachdeva@helpshift.com>
2021-09-05 14:35:12 +0530

    Add splitChunks

--- 10-wp-split-chunks/webpack.config.js
+++ 10-wp-split-chunks/webpack.config.js
@@ -2,10 +2,17 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./10-wp-split-chuks/index.js",
+  entry: "./10-wp-split-chunks/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
   devtool: false,
+
+  optimization: {
+    splitChunks: {
+      chunks: "async",
+      minSize: 0,
+    },
+  },
 };
